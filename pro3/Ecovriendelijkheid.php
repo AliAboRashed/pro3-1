1db6e6095638277f6e311cc05df429c5f8e24af1
AliAboRashed <112874123+AliAboRashed@users.noreply.github.com>
2023-04-13 21:48:53 +0200

    Add files via upload

---
+++ pro3/Ecovriendelijkheid.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Eco Vriendelijkheid</title>
+</head>
+
+<body>
+    
+	<?php include "nav.php" ?>
+    <div class="duur">
+<h1>Eco Vriendelijkheid</h1>
+
+    
+        <h2>Duurzame materialen</h2>
+        <p>Bara fashion maakt enkel gebruik van duurzame materialen. Zo zijn onze prijzen goed betaalbaar en van goede kwaliteit. <br>
+        Het grootste deel van onze materialen is biologisch. Wij maken gebruik van biologisch katoen, biologische linnen, <br>
+        viscose, hennep en lyocell. <br></p>
+
+        <h2>Energiezuinig</h2>
+        <p>Wij bij Bara fashion besparen energie op velen vlakken. Wij hebben geen winkel waardoor wij energie, water en gaskosten besparen. <br>
+        Verder bestellen wij alleen meer voorraad als onze opslag leegraakt. Ook is een deel handgemaakt.</p>
+
+        <h2>Bezorging</h2>
+        <p>Onze bezorging is ook zuinig, wij bezorgen alleen met elektrische voertuigen of voertuigen die rijden op lucht. <br>
+        Bestelt u vanuit een land wat ver verwijderd is van ons magazijn, vervoeren wij uw bestelling wel met de vliegtuig.</p>
+    </div>
+
+
+   <?php include 'footer.php'; ?>
+</body>
+</html>
---
+++ pro3/home.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bara Fashion</title>
+</head>
+<link rel="stylesheet" href="style.css">
+<link rel="website icon" type="png" href="img/Logo.png">
+<body>
+	<?php include "nav.php" ?>
+	<div class="achtergrondfoto"></div>
+	<main>
+		<section>
+			<div class="text-box">
+            <h1>BARA Fashion</h1>
+            <p> Welkom naar onze  website.</p>
+            <a href="shop.php" class="hero-btn">Visit us to know more</a>
+        </div>
+		</section>
+            <section>
+			
+			<div class="pics">
+			    <main class="producten_main">
+                <div class="products">
+                   <div class="section_suits">
+                            <a href="shop1.php"><img src="img/pak2.jpg" class="producten"></a>
+                            <h3 id="h3">Nike Sock Dart Premium</h3>
+                            <p></p>
+                            <span>€89,99</span>
+                        </div>
+
+                            <div class="section_suits">
+                                <a href="shop1.php"><img src="img/pak3.webp" class="producten"></a>
+                                <h3 id="h3">Nike Sock Dart Premium</h3>
+                                <p></p>
+                                <span>€89,99</span>
+                            </div>
+
+                                <div class="section_suits">
+                                    <a href="shop1.php"><img src="img/20.webp" class="producten"></a>
+                                    <h3 id="h3">Nike Sock Dart Premium</h3>
+                                    <p></p>
+                                    <span>€89,99</span>
+                                </div>
+
+                                    <div class="section_suits">
+                                        <a href="shop1.php"><img src="img/pak7.webp" class="producten"></a>
+                                        <h3 id="h3">Nike Sock Dart Premium</h3>
+                                        <p></p>
+                                        <span>€89,99</span>
+                                    </div>
+                        
+                                    
+
+                   
+		</section>
+	</main>
+    
+	<?php include 'footer.php'; ?>
+</body>
+</html>
